94d3dc83a794c19e923a0c1456bf97cef0734a8c
Juan Pablo Rivera Sierra <54080237+Juriveras@users.noreply.github.com>
2021-11-01 18:23:03 -0500

    Created Control, Marca  and TV classes

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,105 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		numTV++;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	
+	public static int getNumTV() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		this.estado = true;
+	}
+	
+	public void turnOff() {
+		this.estado = false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp() {
+		if(canal == 120 || estado == false) {
+			return;
+		}
+		
+		canal++;
+	}
+	
+	public void canalDown() {
+		if(canal == 1 || estado == false) {
+			return;
+		}
+		
+		canal--;
+	}
+	
+	public void volumenUp() {
+		if(volumen == 7 || estado == false) {
+			return;
+		}
+		
+		volumen++;
+	}
+	
+	public void volumenDown() {
+		if(volumen == 1 || estado == false) {
+			return;
+		}
+		
+		volumen--;
+	}
+}
